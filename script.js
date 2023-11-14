1ee3656389178ceb16aac08e61e907a464e976de
manumith26 <144742738+manumith26@users.noreply.github.com>
2023-11-13 20:46:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function addNumbers() {
+            var num1 = parseFloat(document.getElementById('num1').value);
+            var num2 = parseFloat(document.getElementById('num2').value);
+
+            if (!isNaN(num1) && !isNaN(num2)) {
+                var sum = num1 + num2;
+                document.getElementById('result').innerText = "Sum: " + sum;
+            } else {
+                alert("Please enter valid numbers.");
+            }
+        }
